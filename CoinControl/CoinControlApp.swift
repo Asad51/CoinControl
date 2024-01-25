659462c5bf5a1863845c7b08a34a492ceb312498
Md. Asadul Islam <asadul_islam@outlook.com>
2024-01-25 23:38:13 +0600

    Project Initialization
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

---
+++ CoinControl/CoinControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinControlApp.swift
+//  CoinControl
+//
+//  Created by Md. Asadul Islam on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
