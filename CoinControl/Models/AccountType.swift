5743eaa43d67848cd4cb9bb1edd3038f67fcbbc0
Md. Asadul Islam <asadul_islam@outlook.com>
2024-02-15 22:19:15 +0600

    ReviewDog test

---
+++ CoinControl/Models/AccountType.swift
@@ -0,0 +1,14 @@
+//
+//  AccountType.swift
+//  CoinControl
+//
+//  Created by Md. Asadul Islam on 15/2/24.
+//
+
+import Foundation
+
+enum AccountType: String,CaseIterable {
+    case cash
+    case card
+    case accounts//test comment
+}
