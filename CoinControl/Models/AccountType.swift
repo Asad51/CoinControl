e06e3baea8789f21261768a5142e1cffc0113ac6
Md. Asadul Islam <asadul_islam@outlook.com>
2024-02-28 00:25:16 +0600

    Designed record row
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

---
+++ CoinControl/Models/AccountType.swift
@@ -0,0 +1,14 @@
+//
+//  AccountType.swift
+//  CoinControl
+//
+//  Created by Md. Asadul Islam on 17/2/24.
+//
+
+import Foundation
+
+enum AccountType {
+    case cash
+    case card
+    case accounts
+}
