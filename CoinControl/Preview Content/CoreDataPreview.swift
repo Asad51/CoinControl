a505c7a6c731919fe15a2e54a99bfdc385cc035e
Md. Asadul Islam <asadul_islam@outlook.com>
2024-04-03 00:41:24 +0600

    Display records list
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

--- CoinControl/Preview Content/CoreDataPreview.swift
+++ CoinControl/Preview Content/CoreDataPreview.swift
@@ -8,26 +8,27 @@
 import CoreData
 import SwiftUI
 
-struct CoreDataPreview<Content: View, PersistenceModel>: View {
+struct CoreDataPreview<Content: View, PersistenceModel: Hashable>: View {
     private let content: Content
     private let persistenceController: PersistenceController
 
-    init(_ keyPath: KeyPath<PreviewData, (NSManagedObjectContext) -> PersistenceModel>, @ViewBuilder content: (PersistenceModel) -> Content) {
+    // FIXME: Need to fix ambiguous initializers
+//    init(_ keyPath: KeyPath<PreviewData, (NSManagedObjectContext) -> PersistenceModel>, @ViewBuilder content: (PersistenceModel) -> Content) {
+//        persistenceController = PersistenceController(inMemory: true)
+//        let data = PreviewData()
+//        let closure = data[keyPath: keyPath]
+//        let item = closure(persistenceController.viewContext)
+//
+//        self.content = content(item)
+//    }
+
+    init(_ keyPath: KeyPath<PreviewData, (NSManagedObjectContext) -> [PersistenceModel]>, @ViewBuilder content: ([PersistenceModel]) -> Content) {
         persistenceController = PersistenceController(inMemory: true)
         let data = PreviewData()
         let closure = data[keyPath: keyPath]
-        let item = closure(persistenceController.viewContext)
+        let items = closure(persistenceController.viewContext)
 
-        self.content = content(item)
-    }
-
-    init(_ keyPath: KeyPath<PreviewData, (NSManagedObjectContext) -> [PersistenceModel]>, @ViewBuilder content: () -> Content) {
-        persistenceController = PersistenceController(inMemory: true)
-        let data = PreviewData()
-        let closure = data[keyPath: keyPath]
-        _ = closure(persistenceController.viewContext)
-
-        self.content = content()
+        self.content = content(items)
     }
 
     var body: some View {
