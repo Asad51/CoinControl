7cecda6bfa82801aa22d997785fb50e7805a6b24
Md. Asadul Islam <asadul_islam@outlook.com>
2026-04-23 00:16:40 +0600

    Integrate Firebase Crashlytics
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

--- CoinControl/App/AppDelegate.swift
+++ CoinControl/App/AppDelegate.swift
@@ -8,12 +8,14 @@
 import AppCenter
 import AppCenterAnalytics
 import AppCenterCrashes
+import Firebase
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         AppCenter.start(withAppSecret: "d372f15d-68c7-4adf-88fc-a90c644b4d35", services: [
             Crashes.self, Analytics.self])
+        FirebaseApp.configure()
         return true
     }
 }
