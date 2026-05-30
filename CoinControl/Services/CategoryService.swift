e6adb750ade5625fe2116024f2309d3bb7f83a5d
Md. Asadul Islam <asadul_islam@outlook.com>
2026-05-31 00:24:24 +0600

    Add category edit view

--- CoinControl/Services/CategoryService.swift
+++ CoinControl/Services/CategoryService.swift
@@ -9,6 +9,8 @@ import Foundation
 protocol CategoryServiceProtocol {
     func fetchCategories() throws -> [Category]
     func fetchCategories(by type: Int16) throws -> [Category]
+    func addCategory(name: String, icon: String, type: Int16) throws
+    func deleteCategory(_ category: Category) throws
 }
 
 class CategoryService: CategoryServiceProtocol {
@@ -29,5 +31,20 @@ class CategoryService: CategoryServiceProtocol {
         request.predicate = NSPredicate(format: "type == %d", type)
         request.sortDescriptors = [NSSortDescriptor(keyPath: \Category.name, ascending: true)]
         return try context.fetch(request)
+    }
+
+    func addCategory(name: String, icon: String, type: Int16) throws {
+        let category = Category(context: context)
+        category.id = UUID()
+        category.name = name
+        category.icon = icon
+        category.type = type
+        
+        try context.save()
+    }
+
+    func deleteCategory(_ category: Category) throws {
+        context.delete(category)
+        try context.save()
     }
 }
